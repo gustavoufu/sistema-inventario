b7ce25a1168763911d9527cb579b4102120bb533
gustavo <gustavoheriq500@ufu.br>
2026-09-18 15:25:26 -0300

    inicio CRUD sem salvamento de arquivos

--- estudos/openarquivos.py
+++ estudos/openarquivos.py
@@ -1,27 +1,65 @@
 import json
 
+def verificar_int(mensagem): #VERIFICAR INT É LIGADO A UMA MENSAGEM INPUT
+    while True:
+        try:
+            inteiro = int(input(mensagem))
+        except (ValueError, TypeError):
+            print("ERRO, digite um número inteiro válido...")
+            continue
+        else: 
+            return inteiro
+
 lista_ativos = []
 
-while True:
 
-    temp_ativo = {"nome": input("Digite o nome para o ativo: "),
-                  "setor": input("Digite o setor do ativo: "),
-                  "vulnerabilidade": input("Digite a vulnerabilidade do ativo")}
-    lista_ativos.append(temp_ativo)
 
-    escolha = input("Deseja cadastrar mais um ativo? [S/N]")
-    if escolha == "s":
+while True: #EXIBE AS OPCOES DO CRUD
+    opcao = verificar_int("""
+    -=-=-=-=-= OPÇÕES DE ATIVO =-=-=-=-=-=-
+
+    1 - Cadastro de ativos
+    2 - Listar ativos
+    3 - Remover ativos
+    4 - Sair
+
+    Escolha uma opção: """)
+
+    if opcao not in range(1, 5): #AVISA ERRO -> SE COLOCAR OPCAO QUE NAO EXISTE
+        print("")
+        print("ERRO: Essa opção não está disponível...")
         continue
-    if escolha == "n":
-        break
 
+    elif opcao == 1: #COLETA DADOS DO NOVO ATIVO PARA SUBIR NA LISTA DE DICIONARIOS
+        print("")
+        dados_ativo_novo = {"nome": input("Digite um nome para o ativo: "),
+                            "setor": input("Digite o setor responsável: "),
+                            "vulnerabilidade": input("Digite a vulnerabilidade: "),
+                            "severidade": input("Digite um nível de severidade [1 até 5]: ")}
+        lista_ativos.append(dados_ativo_novo)
 
-with open("estudos/Vulnerabilidades.json", "w") as vulne:
-    json.dump(lista_ativos, vulne)
+    elif opcao == 2: #LISTA TODOS OS DICIONARIOS NA LISTA "LISTA_ATIVOS"
+        print("-=-=-=-=-=- ATIVOS CADASTRADOS -=-=-=-=-=-=-")
+        print("")
+        for posicao, ativo in enumerate(lista_ativos):
+            print(f"{posicao + 1} - {ativo['nome']}")
 
-with open("estudos/Vulnerabilidades.json", "r") as vulne:
-    lido = json.load(vulne)
+    elif opcao == 3:
+        print("-=-=-=-=-=- ATIVOS CADASTRADOS -=-=-=-=-=-=-")
+        print("")
+        print("Qual ativo você deseja remover?")
+        print("")
+        for posicao, ativo in enumerate(lista_ativos, start=1):
+                    print(f"{posicao} - {ativo['nome']}")
+        print("")
 
-for pessoa in lido:
-    for k, v in pessoa.items():
-        print(f"A key é {k} e o valor é {v}...")
+        while True: #CRIA UM LOOP PARA SELECIONAR UMA POSICAO DE ATIVO VALIDA PARA EXCLUIR
+            escolha_rem = verificar_int("Escolha um ativo: ")
+            if 1 <= escolha_rem <= len(lista_ativos):
+                del lista_ativos[escolha_rem - 1]
+                break
+            else: 
+                 print("Essa opção não está disponível")
+                 continue
+    elif opcao == 4:
+         break
